a847ddec207bf1dd38d82e5cef5adbb29ab2eff8
ElliottStokes <35205384+ElliottStokes@users.noreply.github.com>
2026-07-12 12:57:49 +0100

    feat(#46): switch to HTTP API (#47)

--- lib/two-rooms-and-a-boom-stack.ts
+++ lib/two-rooms-and-a-boom-stack.ts
@@ -4,17 +4,30 @@ import {Runtime} from 'aws-cdk-lib/aws-lambda';
 import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
 import {PolicyStatement} from 'aws-cdk-lib/aws-iam';
 import {LogGroup, RetentionDays} from 'aws-cdk-lib/aws-logs';
-import {LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway';
+import {
+  CorsHttpMethod,
+  HttpApi,
+  HttpMethod,
+} from 'aws-cdk-lib/aws-apigatewayv2';
+import {HttpLambdaIntegration} from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+
+import type {HttpApiProps} from 'aws-cdk-lib/aws-apigatewayv2';
 
 const AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID ?? '';
 const AWS_REGION = process.env.AWS_REGION ?? '';
 const DSQL_CLUSTER_ID = process.env.DSQL_CLUSTER_ID ?? '';
 const DSQL_CLUSTER_ENDPOINT = process.env.DSQL_CLUSTER_ENDPOINT ?? '';
 
-const DEFAULT_CORS_OPTIONS = {
-  defaultCorsPreflightOptions: {
+const DEFAULT_CORS_OPTIONS: HttpApiProps = {
+  corsPreflight: {
     allowOrigins: ['*'],
-    allowMethods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowMethods: [
+      CorsHttpMethod.GET,
+      CorsHttpMethod.POST,
+      CorsHttpMethod.PATCH,
+      CorsHttpMethod.DELETE,
+      CorsHttpMethod.OPTIONS,
+    ],
     allowHeaders: ['Content-Type', 'Authorization'],
   },
 };
@@ -23,67 +36,86 @@ export class TwoRoomsAndABoomStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const restApi = new RestApi(
+    const httpApi = new HttpApi(
       this,
       'two-rooms-and-a-boom-api-gateway',
       DEFAULT_CORS_OPTIONS,
     );
-    const playerPath = restApi.root.addResource('player', DEFAULT_CORS_OPTIONS);
-    playerPath
-      .addResource('all')
-      .addMethod('GET', createEndpoint(this, 'listAllPlayers'));
-    playerPath
-      .addResource('check')
-      .addResource('{username}')
-      .addMethod('GET', createEndpoint(this, 'checkPlayerCredentials'));
-    playerPath.addMethod('POST', createEndpoint(this, 'registerNewPlayer'));
-    playerPath
-      .addResource('card')
-      .addResource('{cardId}')
-      .addMethod(
-        'GET',
-        createEndpoint(this, 'getCardImageUrl', [
-          new PolicyStatement({
-            actions: ['s3:GetObject'],
-            resources: ['arn:aws:s3:::two-rooms-and-a-boom/cards/*'],
-          }),
-        ]),
-      );
 
-    const playerPathPlayerId = playerPath.addResource('{playerId}');
-    playerPathPlayerId.addMethod(
-      'GET',
-      createEndpoint(this, 'getPlayerDetails'),
-    );
-    playerPathPlayerId.addMethod(
-      'DELETE',
-      createEndpoint(this, 'deletePlayer'),
-    );
+    httpApi.addRoutes({
+      path: '/player/all',
+      methods: [HttpMethod.GET],
+      integration: createEndpoint(this, 'listAllPlayers'),
+    });
+    httpApi.addRoutes({
+      path: '/player/check/{username}',
+      methods: [HttpMethod.GET],
+      integration: createEndpoint(this, 'checkPlayerCredentials'),
+    });
+    httpApi.addRoutes({
+      path: '/player',
+      methods: [HttpMethod.POST],
+      integration: createEndpoint(this, 'registerNewPlayer'),
+    });
+    httpApi.addRoutes({
+      path: '/player/card/{cardId}',
+      methods: [HttpMethod.GET],
+      integration: createEndpoint(this, 'getCardImageUrl', [
+        new PolicyStatement({
+          actions: ['s3:GetObject'],
+          resources: ['arn:aws:s3:::two-rooms-and-a-boom/cards/*'],
+        }),
+      ]),
+    });
+    httpApi.addRoutes({
+      path: '/player/{playerId}',
+      methods: [HttpMethod.GET],
+      integration: createEndpoint(this, 'getPlayerDetails'),
+    });
+    httpApi.addRoutes({
+      path: '/player/{playerId}',
+      methods: [HttpMethod.DELETE],
+      integration: createEndpoint(this, 'deletePlayer'),
+    });
 
-    const cardsPath = restApi.root.addResource('cards');
-    cardsPath
-      .addResource('set')
-      .addMethod('PATCH', createEndpoint(this, 'setActiveCards'));
-    cardsPath
-      .addResource('clear')
-      .addMethod('PATCH', createEndpoint(this, 'clearActiveCards'));
-    cardsPath
-      .addResource('deal')
-      .addMethod('PATCH', createEndpoint(this, 'assignPlayers'));
+    httpApi.addRoutes({
+      path: '/cards',
+      methods: [HttpMethod.PATCH],
+      integration: createEndpoint(this, 'setActiveCards'),
+    });
+    httpApi.addRoutes({
+      path: '/cards/clear',
+      methods: [HttpMethod.PATCH],
+      integration: createEndpoint(this, 'clearActiveCards'),
+    });
+    httpApi.addRoutes({
+      path: '/cards/deal',
+      methods: [HttpMethod.PATCH],
+      integration: createEndpoint(this, 'assignPlayers'),
+    });
 
-    const gamePath = restApi.root.addResource('game');
-    gamePath
-      .addResource('start')
-      .addMethod('PATCH', createEndpoint(this, 'startGame'));
-    gamePath
-      .addResource('reveal')
-      .addMethod('PATCH', createEndpoint(this, 'revealCards'));
-    gamePath
-      .addResource('end')
-      .addMethod('PATCH', createEndpoint(this, 'endGame'));
-    gamePath.addMethod('GET', createEndpoint(this, 'getGameState'));
+    httpApi.addRoutes({
+      path: '/game/start',
+      methods: [HttpMethod.PATCH],
+      integration: createEndpoint(this, 'startGame'),
+    });
+    httpApi.addRoutes({
+      path: '/game/reveal',
+      methods: [HttpMethod.PATCH],
+      integration: createEndpoint(this, 'revealCards'),
+    });
+    httpApi.addRoutes({
+      path: '/game/end',
+      methods: [HttpMethod.PATCH],
+      integration: createEndpoint(this, 'endGame'),
+    });
+    httpApi.addRoutes({
+      path: '/game',
+      methods: [HttpMethod.GET],
+      integration: createEndpoint(this, 'getGameState'),
+    });
 
-    new CfnOutput(this, 'ApiGatewayUrl', {value: restApi.url});
+    new CfnOutput(this, 'ApiGatewayUrl', {value: httpApi.url || ''});
   }
 }
 
@@ -91,7 +123,7 @@ function createEndpoint(
   scope: Construct,
   functionName: string,
   permissions?: PolicyStatement[],
-) {
+): HttpLambdaIntegration {
   const endpointFunction = new NodejsFunction(scope, functionName, {
     functionName,
     runtime: Runtime.NODEJS_22_X,
@@ -117,5 +149,5 @@ function createEndpoint(
       endpointFunction.addToRolePolicy(permission),
     );
   }
-  return new LambdaIntegration(endpointFunction);
+  return new HttpLambdaIntegration(functionName, endpointFunction);
 }
